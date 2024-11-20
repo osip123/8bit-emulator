aad1e76ceeffabb54dd71754c56147bf70d1441c
osip123 <endocrinology90px@gmail.com>
2024-11-20 15:36:27 +0300

    test math inst

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as _ from 'lodash';
 import { createRegisters, register } from './cpu/registers';
 import { regarrds } from './cpu/regaddr';
 import CPU from './cpu/cpu';
+import XOR from './instrucrion/instProgramCounterStackControl';
 
 function main() {
     let registers: register[] = createRegisters(regarrds);
@@ -9,12 +10,17 @@ function main() {
     registers[1].write("0001");
     registers[2].write("0101");
     registers[5].write("0001");
+    
+    registers[0].write("0101");
+    registers[1].write("0111");
 
     let cpu = new CPU(registers)
 
     console.log(cpu.registers);
     cpu.execute(["0001", "0x04", "010011"]);
     console.log(cpu.registers);
+
+    console.log(XOR(0x01, 0x02, registers))
 }
 
 main();
--- src/instrucrion/instProgramCounterStackControl.ts
+++ src/instrucrion/instProgramCounterStackControl.ts
@@ -1,5 +1,6 @@
 import { register } from "../cpu/registers";
 import getRegisterIndex from "../cpu/registersutiles";
+import { toInteger } from 'lodash';
 
 export function JMP(addr: number, registers: register): void {
     
@@ -15,9 +16,8 @@ export default function XOR(addr1: number, addr2: number , registers: register[]
     const index1  = getRegisterIndex(addr1)
     const index2 = getRegisterIndex(addr2)
 
-    if(registers[index1].read() < registers[index2].read()){
+    if(toInteger(registers[index1].read()) < toInteger(registers[index2].read())){
         return registers[index1].arrd
     }
     else return registers[index2].arrd
 }
-
