133b4ade59306eaace0f0abc7a89373c16a1cd78
osip123 <endocrinology90px@gmail.com>
2024-11-22 16:15:35 +0300

    base cpu structure

--- src/cpu/cpu.ts
+++ src/cpu/cpu.ts
@@ -1,5 +1,25 @@
 import { toInteger } from 'lodash';
+import { register } from './registers';
+import Decoder from '../decoder/decoder';
 
 export default class Cpu {
-    
+
+    private registers: register[];
+    private decoder = new Decoder();
+    private cmds: string[];
+
+    constructor(registers: register[]) {
+        this.registers = registers;
+        this.cmds = [];
+    }
+
+    private  execute(){
+
+    }
+
+
+    public run(){
+
+    }
+
 }
